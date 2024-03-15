eaf10715a6fbb30da6ad28847fc58ec7c3861a42
lskErixon <erik.lodkorikh@gmail.com>
2024-03-15 13:26:03 +0100

    added DrawBoard

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -2,17 +2,9 @@ package org.example;
 
 public class TicTacToe {
     private int[][] board = new int[3][3];
-
-
-
-
-
-
-
     public int[][] getBoard() {
         return board;
     }
-
     public void setBoard(int[][] board) {
         this.board = board;
     }
